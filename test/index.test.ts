0eb679a8f7b074c32646561190950a37b1e4ef49
Anthony Fu <anthonyfu117@hotmail.com>
2022-05-07 10:55:33 +0800

    chore: add more tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,54 +1,146 @@
+/* eslint-disable no-template-curly-in-string */
 import { parse } from 'acorn'
-import { describe, expect, it } from 'vitest'
+import { expect, test } from 'vitest'
 import { stripLiteral } from '../src'
 
-describe('works', () => {
-  function executeWithVerify(code: string) {
-    const result = stripLiteral(code)
+function executeWithVerify(code: string) {
+  code = code.trim()
+  const result = stripLiteral(code)
 
-    for (let i = 0; i < result.length; i++) {
-      if (!result[i].match(/\s/))
-        expect(result[i]).toBe(code[i])
-    }
-
-    expect(result.length).toBe(code.length)
-
-    // make sure no syntax errors
-    parse(result, { ecmaVersion: 'latest', sourceType: 'module' })
-
-    return result
+  for (let i = 0; i < result.length; i++) {
+    if (!result[i].match(/\s/))
+      expect(result[i]).toBe(code[i])
   }
 
-  it('exported', () => {
-    expect(executeWithVerify(`
-    // comment1
-    const a = 'aaaa'
-    /* comment2 */
-    const b = "bbbb"
-    /*
-      // comment3
-    */
-    /* // comment3 */
-    // comment 4 /* comment 5 */
-    const c = \`ccc\${a}\`
+  expect(result.length).toBe(code.length)
 
-    let d = /re\\\\ge/g
-  `))
-      .toMatchInlineSnapshot(`
-        "
-                       
-            const a = '    '
-                          
-            const b = \\"    \\"
-              
-                         
-              
-                             
-                                        
-            const c = \`   \${a}\`
-        
-            let d = /re\\\\\\\\ge/g
-          "
-      `)
-  })
+  // make sure no syntax errors
+  parse(result, { ecmaVersion: 'latest', sourceType: 'module' })
+
+  return result
+}
+
+test('works', () => {
+  expect(executeWithVerify(`
+// comment1
+const a = 'aaaa'
+/* comment2 */
+const b = "bbbb"
+/*
+  // comment3
+*/
+/* // comment3 */
+// comment 4 /* comment 5 */
+const c = \`ccc\${a}\`
+
+let d = /re\\\\ge/g
+    `)).toMatchSnapshot()
+})
+
+test('escape character', () => {
+  expect(executeWithVerify(`
+'1\\'1'
+"1\\"1"
+"1\\"1\\"1"
+"1\\'1'\\"1"
+"1'1'"
+"1'\\'1\\''\\"1\\"\\""
+'1"\\"1\\""\\"1\\"\\"'
+'""1""'
+'"""1"""'
+'""""1""""'
+"''1''"
+"'''1'''"
+"''''1''''"
+  `)).toMatchSnapshot()
+})
+
+test('regexp affect', () => {
+  expect(executeWithVerify(`
+[
+  /'/,
+  '1',
+  /"/,
+  "1"
+]
+  `)).toMatchSnapshot()
+})
+
+test('strings comment nested', () => {
+  expect(executeWithVerify(`
+// comment 1 /* " */
+const a = "a //"
+// comment 2 /* " */
+  `)).toMatchSnapshot()
+
+  expect(executeWithVerify(`
+// comment 1 /* ' */
+const a = "a //"
+// comment 2 /* ' */
+  `)).toMatchSnapshot()
+
+  expect(executeWithVerify(`
+// comment 1 /* \` */
+const a = "a //"
+// comment 2 /* \` */
+  `)).toMatchSnapshot()
+
+  expect(executeWithVerify(`
+const a = "a //"
+console.log("console")
+  `)).toMatchSnapshot()
+
+  expect(executeWithVerify(`
+const a = "a /*"
+console.log("console")
+const b = "b */"
+  `)).toMatchSnapshot()
+
+  expect(executeWithVerify(`
+const a = "a ' "
+console.log("console")
+const b = "b ' "
+  `)).toMatchSnapshot()
+
+  expect(executeWithVerify(`
+const a = "a \` "
+console.log("console")
+const b = "b \` "
+  `)).toMatchSnapshot()
+})
+
+test('template string nested', () => {
+  let str = '`aaaa`'
+  expect(executeWithVerify(str)).toMatchInlineSnapshot('"`    `"')
+
+  str = '`aaaa` `aaaa`'
+  expect(executeWithVerify(str)).toMatchInlineSnapshot('"`    ` `    `"')
+
+  str = '`aa${a}aa`'
+  expect(executeWithVerify(str)).toMatchInlineSnapshot('"`  \${a}  `"')
+
+  str = '`aa${a + `a` + a}aa`'
+  expect(executeWithVerify(str)).toMatchInlineSnapshot('"`  \${a + ` ` + a}  `"')
+
+  str = '`aa${a + `a` + a}aa` `aa${a + `a` + a}aa`'
+  expect(executeWithVerify(str)).toMatchInlineSnapshot('"`  \${a + ` ` + a}  ` `  \${a + ` ` + a}  `"')
+
+  str = '`aa${a + `aaaa${c + (a = {b: 1}) + d}` + a}aa`'
+  expect(executeWithVerify(str)).toMatchInlineSnapshot('"`  \${a + `    \${c + (a = {b: 1}) + d}` + a}  `"')
+
+  str = '`aa${a + `aaaa${c + (a = {b: 1}) + d}` + a}aa` `aa${a + `aaaa${c + (a = {b: 1}) + d}` + a}aa`'
+  expect(executeWithVerify(str)).toMatchInlineSnapshot('"`  \${a + `    \${c + (a = {b: 1}) + d}` + a}  ` `  \${a + `    \${c + (a = {b: 1}) + d}` + a}  `"')
+})
+
+test('backtick escape', () => {
+  const str = [
+    'this.error(`\\`new URL(url, import.meta.url)\\` is not supported in SSR.`)',
+    'this.error(`\\\\`)',
+    'this.error(`\\``)',
+  ].join('\n')
+  expect(executeWithVerify(str)).toMatchInlineSnapshot(`
+    "this.error(\`                                                          \`)
+    this.error(\`  \`)
+    this.error(\`  \`)"
+  `)
 })
