b3e538ba257804fc9bff516e6f8ba9f9fb2006b4
Anthony Fu <anthonyfu117@hotmail.com>
2022-09-09 22:07:21 +0800

    chore: chore lint

--- bench/index.bench.ts
+++ bench/index.bench.ts
@@ -1,19 +1,19 @@
-import { describe, bench } from 'vitest'
-import { stripLiteralAcorn, stripLiteralRegex } from '../src'
 import { readFile } from 'fs/promises'
+import { bench, describe } from 'vitest'
+import { stripLiteralAcorn, stripLiteralRegex } from '../src'
 
 const modules = {
   'vue-global': './node_modules/vue/dist/vue.runtime.global.js',
-  'three': './node_modules/three/build/three.module.js'
+  'three': './node_modules/three/build/three.module.js',
 }
 
-Object.entries(modules).map(([name, path]) => {
-  describe('bench ' + name, async () => {
-    let code = await readFile(path, 'utf-8')
-    bench('regex', ()=>{
+Object.entries(modules).forEach(([name, path]) => {
+  describe(`bench ${name}`, async () => {
+    const code = await readFile(path, 'utf-8')
+    bench('regex', () => {
       stripLiteralRegex(code)
     })
-    bench('acorn', ()=>{
+    bench('acorn', () => {
       stripLiteralAcorn(code)
     })
   })
