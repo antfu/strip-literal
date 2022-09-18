dbd69f3b43dc8ffb445dc18d944b69480cfb72a4
chenying <1770365139@qq.com>
2022-09-18 15:33:15 +0800

    chore: lint

--- test/index.test.ts
+++ test/index.test.ts
@@ -192,10 +192,10 @@ test('forgiving', () => {
 test('// in string', () => {
   const str = [
     'const url= `http://www.xx.com`;',
-    `const url1= 'http://www.xx.com';`,
+    'const url1= \'http://www.xx.com\';',
     'onMounted(() => console.log(123))',
     'const str = `hello world`',
-    '// Notes'
+    '// Notes',
   ].join('\n')
 
   expect(executeWithVerify(str)).toMatchInlineSnapshot(`
