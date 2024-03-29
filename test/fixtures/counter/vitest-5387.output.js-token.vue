641b7a13b5a2bf8b7dd22d1f3cd971f27e52e68b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-29 13:16:12 +0900

    test: restore regex and compare

---
+++ test/fixtures/counter/vitest-5387.output.js-token.vue
@@ -0,0 +1,12 @@
+<template>
+  <SomeOtherComponent
+    :some-prop=""
+      testValue
+        ? '                                               '
+        : '                                 '
+    ""
+  >
+    <div>{{ count }} x {{ times }} = {{ result }}</div>
+    <button @click="          ">x1</button>
+  </SomeOtherComponent>
+</template>
